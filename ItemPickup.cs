36714618a5b37735edff4668f4667e9b97375563
Alicia <aliciapm89@educastur.es>
2024-04-08 12:18:50 +0200

    item-pickup
    recoger objeto

---
+++ ItemPickup.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+    public Item item;
+
+    public void Pickup()
+    {
+        InventoryManager.Instance.Add(item);
+        Destroy(gameObject);
+    }
+}
