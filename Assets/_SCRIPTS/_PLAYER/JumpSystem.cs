10121ffca3c89f3358adec9c04a80958c4ea1cfc
aliciapm89 <aliciapm89@educastur.es>
2024-05-06 13:55:17 +0200

    Entrega

--- Assets/_SCRIPTS/_PLAYER/JumpSystem.cs
+++ Assets/_SCRIPTS/_PLAYER/JumpSystem.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 //comentareio del 6 de mayo
+//me gustan las patatas fritas
 public class JumpSystem : MonoBehaviour
 {
     PlayerController player;
