33aac4e70d075191a7e93287f4a7969613f8d3e0
aliciapm89 <aliciapm89@educastur.es>
2024-05-06 11:34:16 +0200

    Update JumpSystem.cs

--- Assets/_SCRIPTS/JumpSystem.cs
+++ Assets/_SCRIPTS/JumpSystem.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//comentareio del 6 de mayo
 public class JumpSystem : MonoBehaviour
 {
     PlayerController player;
